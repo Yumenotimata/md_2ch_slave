4fe38f5185265f58248050e0055abbd408993c5a
Yumenotimata <kouyayamaoka@gmail.com>
2023-05-30 21:50:00 +0900

    first commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=md_2ch_slave.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/md_2ch_slave.X.production.hex
