c7c7d0e1a359b7026b1818bcbe499457f767fc4c
Yumenotimata <kouyayamaoka@gmail.com>
2023-06-01 13:53:57 +0900

    add msi init

---
+++ pic.c
@@ -0,0 +1,31 @@
+#include <p33CH64MP505S1.h>
+
+#include "pic.h"
+#include "pwm.h"
+
+void pic_init(void)
+{
+    clock_init();
+    pin_init();
+    pwm_init();
+}
+
+void clock_init(void)
+{
+    CLKDIVbits.PLLPRE = 1;
+    PLLFBDbits.PLLFBDIV = 100;
+    PLLDIVbits.POST1DIV = 4;
+    PLLDIVbits.POST2DIV = 1;
+    __builtin_write_OSCCONH(0x03);
+    __builtin_write_OSCCONL(OSCCON | 0x01);
+    while (OSCCONbits.OSWEN != 0);
+    while (OSCCONbits.LOCK!= 1);
+}
+
+void pin_init(void)
+{
+    TRISCbits.TRISC10 = 0;
+    TRISCbits.TRISC11 = 0;
+    TRISCbits.TRISC4 = 0;
+    TRISCbits.TRISC5 = 0;
+}
